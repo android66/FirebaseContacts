a21aae6afc1921be2c2c4790fecbff4d30539092
Hank Tom <hanktom223@gmail.com>
2016-04-28 22:45:09 +0800

    ListView連結Firebase

--- app/src/main/java/com/tom/firebasecontacts/MainActivity.java
+++ app/src/main/java/com/tom/firebasecontacts/MainActivity.java
@@ -2,6 +2,13 @@ package com.tom.firebasecontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1);
+        list.setAdapter(adapter);
+        Firebase.setAndroidContext(this);
+        String url = "https://contacts-example.ﬁrebaseio.com/contacts";
+        new Firebase(url).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                adapter.add(
+                        (String) dataSnapshot.child("name").getValue());
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                adapter.remove(
+                        (String) dataSnapshot.child("name").getValue());
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
